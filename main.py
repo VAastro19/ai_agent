353d536b548e6a853fe510b751ea958dd5bf5b9f
VAastro19 <aetereth.novum@protonmail.com>
2025-11-10 12:25:41 +0100

    added loop to keep conversation going and memory

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    messages = []
+    iteration = 0
 
     system_prompt = """
         You are a helpful AI coding agent.
@@ -20,61 +22,74 @@ def main():
 
         - List files and directories
 
-        - schema_get_file_content
+        - Get file's content
 
-        - schema_run_python_file
+        - Run python files
 
-        - schema_write_file
+        - Write data into files
 
         All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+        Once you've laid out your plan, perform necessary function calls to complete the required task. Once the task is finished, report the results and prove that
+        everything works as intended.
         """
     available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-        schema_get_file_content,
-        schema_run_python_file,
-        schema_write_file,
-    ]
-)
-    
-    if len(sys.argv) > 1:
-        flags = []
-        for arg in sys.argv:
-            if arg.startswith("--"): flags.append(arg)
-            elif arg != sys.argv[0]: prompt = arg
-        messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
-        response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=messages,
-            config=types.GenerateContentConfig(
-                tools=[available_functions],
-                system_instruction=system_prompt
-            ),
-        )
-    else:
-        print("Error: prompt not provided!")
-        sys.exit(1)
-    
-    is_verbose = "--verbose" in flags
+        function_declarations=[
+            schema_get_files_info,
+            schema_get_file_content,
+            schema_run_python_file,
+            schema_write_file,
+        ]
+    )
+    while iteration < 20:
+        try:
+            if len(sys.argv) > 1:
+                flags = []
+                for arg in sys.argv:
+                    if arg.startswith("--"): flags.append(arg)
+                    elif arg != sys.argv[0]: prompt = arg
+                messages.append(types.Content(role="user", parts=[types.Part(text=prompt)]))
+                response = client.models.generate_content(
+                    model="gemini-2.0-flash-001",
+                    contents=messages,
+                    config=types.GenerateContentConfig(
+                        tools=[available_functions],
+                        system_instruction=system_prompt
+                    ),
+                )
+            else:
+                print("Error: prompt not provided!")
+                sys.exit(1)
+        
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+        except Exception as e:
+            print(f"Error: {e}")
+        
+        is_verbose = "--verbose" in flags
 
-    if is_verbose:
-        print(f"User prompt: {prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if is_verbose:
+            print(f"User prompt: {prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    if response.function_calls:
-        for call in response.function_calls:
-            function_call_result = call_function(call, verbose=is_verbose)
+        if response.function_calls:
+            for call in response.function_calls:
+                function_call_result = call_function(call, verbose=is_verbose)
 
-            if not (function_call_result.parts 
-                    and hasattr(function_call_result.parts[0], "function_response") 
-                    and function_call_result.parts[0].function_response.response):
-                raise RuntimeError("Missing function response")
+                if not (function_call_result.parts 
+                        and hasattr(function_call_result.parts[0], "function_response") 
+                        and function_call_result.parts[0].function_response.response):
+                    raise RuntimeError("Missing function response")
+                
+                messages.append(types.Content(role="user", parts=[types.Part(text=str(function_call_result.parts[0].function_response.response))]))
 
-            if is_verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(response.text)
+                if is_verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+        else:
+            print(response.text)
+            break
+
+        iteration += 1
 
 if __name__ == "__main__":
     main()
